bddfb94131ace08c729daabb77bd1417fc8a8978
jawaabi123 <115395173+jawaabi123@users.noreply.github.com>
2022-10-15 17:50:52 +0530

    Create Quesn1.cpp

---
+++ cpp/Quesn1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+       
+       for(int i=2000;i<=3200;i++)
+       {
+           if((i%7)==0 && (i%5)!=0)
+           {
+               cout<<i<<",";
+           }
+       }
+       return 0;
+}
